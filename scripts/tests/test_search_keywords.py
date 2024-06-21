b79407e7b30340b616da3f96e3ecfab719255fa1
bouj1113 <bouj1113@dinf-0051-27b.DInf.fsci.usherbrooke.ca>
2024-06-21 09:41:30 -0400

    update test_search_keywords test file: add test for stemmig

--- scripts/tests/test_search_keywords.py
+++ scripts/tests/test_search_keywords.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import pytest
+from scil_search_keywords import _stem_keywords, _stem_text
 
 def test_help_option(script_runner):
     ret = script_runner.run('scil_search_keywords.py', '--help')
@@ -20,3 +21,13 @@ def test_verbose_option(script_runner):
 def test_not_find(script_runner):
     ret = script_runner.run('scil_search_keywords.py', 'toto')
     assert ret.success
+
+def test_stem_keywords():
+    keywords = ['converting', 'conversion', 'convert']
+    expected_stems = ['convert', 'convers', 'convert']
+    assert _stem_keywords(keywords) == expected_stems
+
+def test_stem_text():
+    text = 'converting conversion convert'
+    expected_stemmed_text = 'convert convers convert'
+    assert _stem_text(text) == expected_stemmed_text
