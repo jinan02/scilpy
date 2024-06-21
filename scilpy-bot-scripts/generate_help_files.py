a896e1455d80c92eb5ecfdbf55bdf560a0cd4fd2
bouj1113 <bouj1113@dinf-0051-27b.DInf.fsci.usherbrooke.ca>
2024-06-21 09:11:50 -0400

    add comments and update docstring

--- scilpy-bot-scripts/generate_help_files.py
+++ scilpy-bot-scripts/generate_help_files.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This script iterates over all Python scripts in the 'scripts' directory, runs each script with the '--h' flag to generate help text, and saves the output to corresponding hidden files in the '.hidden' directory.
+This script iterates over all Python scripts in the 'scripts' directory, 
+runs each script with the '--h' flag to generate help text, 
+and saves the output to corresponding hidden files in the '.hidden' directory.
 
-By doing this, we can precompute the help outputs for each script, which can be useful for faster searches or documentation purposes.
+By doing this, we can precompute the help outputs for each script, 
+which can be useful for faster searches.
 
-Scripts that should be skipped:
-
-- '**init**.py'
-- 'scil_search_keywords.py'
+If a help file already exists for a script, the script is skipped, 
+and the existing help file is left unchanged.
 
 The help output is saved in a hidden directory to avoid clutter in the main scripts directory.
 """
 
-import os
 import subprocess
 from pathlib import Path
 
 
 
-# Directory where your scripts are located
 scripts_dir = Path('scripts/')
 
 # Hidden directory to store help files
